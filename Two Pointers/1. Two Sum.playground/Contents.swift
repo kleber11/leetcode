bec48534efdf1d96df55ce5f0e9d0c82636319c7
Vladyslav Vcherashnii <vvcherashnii@anonyome.com>
2024-09-27 22:00:20 -0700

    added basic two pointers problem

---
+++ Two Pointers/1. Two Sum.playground/Contents.swift
@@ -0,0 +1,43 @@
+/*
+# Two Sum Problem - https://leetcode.com/problems/two-sum/description/?envType=problem-list-v2&envId=mg1j2op3
+
+**Difficulty:** Easy
+**Solved:** Yes
+
+## Problem Overview:
+Given an array of integers `nums` and an integer `target`, the task is to **return the indices** of the two numbers such that they add up to the target.
+- Each input will have **exactly one solution**.
+- You may **not use the same element twice**.
+- The answer can be returned in any order.
+
+## Example Scenarios:
+
+### Example 1:
+**Input:**
+nums = [2, 7, 11, 15], target = 9
+**Output:**
+[0, 1]
+**Explanation:**
+`nums[0] + nums[1] == 9`, therefore we return [0, 1].
+
+### Example 2:
+**Input:**
+nums = [3, 2, 4], target = 6
+**Output:**
+[1, 2]
+
+### Example 3:
+**Input:**
+nums = [3, 3], target = 6
+**Output:**
+[0, 1]
+
+## Constraints:
+- The array `nums` will have a length between **2 and 10^4**.
+- Each element in the array `nums[i]` will be between **-10^9 and 10^9**.
+- The target value `target` will also be between **-10^9 and 10^9**.
+- **Only one valid answer exists**.
+
+## Follow-Up:
+Can you find a solution with a **time complexity better than O(n²)**?
+*/
