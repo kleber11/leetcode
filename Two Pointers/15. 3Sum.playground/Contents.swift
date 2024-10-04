67dd09432139754c5719c877f40393cae35b9e4d
Vladyslav Vcherashnii <vvcherashnii@anonyome.com>
2024-10-03 18:36:35 -0700

    Solved: 15. 3Sum

--- Two Pointers/15. 3Sum.playground/Contents.swift
+++ Two Pointers/15. 3Sum.playground/Contents.swift
@@ -40,5 +40,47 @@
 */
 
 func threeSum(_ nums: [Int]) -> [[Int]] {
-    return [[]]
+    // Result array
+    var result: [[Int]] = []
+    // Need to sort input array first
+    var nums = nums.sorted()
+
+    for (index, number) in nums.enumerated() {
+        if index > 0 && number == nums[index - 1] {
+            continue // We must check if element is not the first element in array. So we can perform `nums[index-1]` operation.
+        }
+
+        // Left and Right pointers
+        var leftP = index + 1
+        var rightP = nums.count - 1
+
+        // We can not have duplicates, so pointers must not meet.
+        while leftP < rightP {
+            let sum = number + nums[leftP] + nums[rightP]
+
+            if sum == 0 {
+                result.append([number, nums[leftP], nums[rightP]])
+
+                // By performing `leftP < rightP` we are ensuring by shifting pointers left and right pointers will not meet.
+                while leftP < rightP && nums[leftP] == nums[leftP + 1] {
+                    leftP += 1
+                }
+
+                while leftP < rightP && nums[rightP] == nums[rightP - 1] {
+                    rightP += 1
+                }
+
+                leftP += 1
+                rightP -= 1
+            } else if sum > 0 {
+                rightP -= 1
+            } else {
+                leftP += 1
+            }
+        }
+    }
+
+    return result
 }
+
+print(threeSum([-1, 0, 1, 2, -1, -4]))
