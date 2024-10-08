2f672fc0643e0b4df070f291732a7baa1d6b2cc1
Vladyslav Vcherashnii <vvcherashnii@anonyome.com>
2024-10-07 20:10:55 -0700

    middle of linked list

---
+++ Fast & Slow Pointers/876. Middle of the Linked List-10-7-2024.playground/Contents.swift
@@ -0,0 +1,48 @@
+/**
+ ## 876. Middle of the Linked List**
+
+ **Topics:** Linked List, Two Pointers
+
+ Given the **head** of a singly linked list, return the **middle node** of the linked list.
+
+ If there are **two middle nodes**, return the **second middle node**.
+
+ ## Example 1:
+ **Input:** `head = [1,2,3,4,5]`
+ **Output:** `[3,4,5]`
+ **Explanation:** The middle node of the list is node `3`.
+
+ ## Example 2:
+ **Input:** `head = [1,2,3,4,5,6]`
+ **Output:** `[4,5,6]`
+ **Explanation:** Since the list has two middle nodes with values `3` and `4`, we return the second one.
+
+ ## Constraints:
+ - The number of nodes in the list is in the range **[1, 100]**.
+ - **1 <= Node.val <= 100**
+ */
+
+public class ListNode {
+    public var val: Int
+    public var next: ListNode?
+    public init() { self.val = 0; self.next = nil; }
+    public init(_ val: Int) { self.val = val; self.next = nil; }
+    public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+}
+
+
+func middleNode(_ head: ListNode?) -> ListNode? {
+    // Define `fast` and `slow` pointers.
+    var slowP = head
+    var fastP = head
+
+    // Iterate until `fast` reaches end
+    // At this point `slow` will point exactly at middle of list.
+    while fastP != nil, fastP?.next != nil {
+        fastP = fastP?.next?.next
+        slowP = slowP?.next
+    }
+
+    // Return `slow` pointer.
+    return slowP
+}
