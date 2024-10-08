ad2f22849e0d4ef533bb1c8934a999f6f4993ede
Vladyslav Vcherashnii <vvcherashnii@anonyome.com>
2024-10-08 09:02:16 -0700

    Solved palindrome linked list

--- Fast & Slow Pointers/234. Palindrome Linked List-10-7-2024.playground/Contents.swift
+++ Fast & Slow Pointers/234. Palindrome Linked List-10-7-2024.playground/Contents.swift
@@ -9,7 +9,7 @@
  **palindrome** or **false** otherwise.
 
  ## Example 1:
- **Input:** `head = [1,2,2,1`
+ **Input:** `head = [1,2,2,1]`
  **Output:** `true`
 
  ## Example 2:
@@ -33,5 +33,45 @@ public class ListNode {
 }
 
 func isPalindrome(_ head: ListNode?) -> Bool {
+    guard head != nil, head?.next != nil else { return true }
+    // Initialize `fast` and `slow` pointers
+    var fastP = head
+    var slowP = head
+
+    // Iterate throught list and determine
+    // where the second half of list begins
+    while fastP != nil, fastP?.next != nil {
+        fastP = fastP?.next?.next
+        slowP = slowP?.next
+    }
+
+    // Once above loop has finished, we can reverse
+    // second part of list where `slowP` ended up
+    var reversedList = reverseLinkedList(slowP)
+    var initialListP = head
+
+    // Iterate through both lists and compare its values
+    while reversedList != nil {
+        if initialListP?.val != reversedList?.val {
+            return false
+        }
+        reversedList = reversedList?.next
+        initialListP = initialListP?.next
+    }
     return true
 }
+
+func reverseLinkedList(_ listNode: ListNode?) -> ListNode? {
+    var current = listNode
+    var prev: ListNode? = nil
+    while current != nil {
+        let next = current?.next
+        current?.next = prev
+        prev = current
+        current = next
+    }
+    return prev
+}
+
+let node = ListNode(1, .init(2, .init(3, .init(3, .init(2, .init(2))))))
+isPalindrome(node)
